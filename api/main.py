ec4765aee89ff2bbd2fae3dc9b9c9f1b84ad7218
橋爪天真 <81737141+kyoto-kanko@users.noreply.github.com>
2022-05-30 16:41:45 +0900

    cors回避

--- api/main.py
+++ api/main.py
@@ -1,9 +1,20 @@
 # https://fastapi.tiangolo.com/ja/tutorial/first-steps/
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from api.routers import task, done
 
 # FastAPIをインスタンス化
 app = FastAPI()
+
+# CORS回避
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(task.router)
 app.include_router(done.router)
