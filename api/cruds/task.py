3b1858539c36818196f798b1be13b39a1e37cee8
橋爪天真 <81737141+kyoto-kanko@users.noreply.github.com>
2022-05-30 18:19:13 +0900

    422エラー回避

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -8,7 +8,7 @@ import api.schemas.task as task_schema
 
 
 async def create_task(
-    db: AsyncSession, task_create: task_schema.TaskCreateResponse
+    db: AsyncSession, task_create: task_schema.TaskCreate
 ) -> task_model.Task:
     task = task_model.Task(**task_create.dict())
     db.add(task)
